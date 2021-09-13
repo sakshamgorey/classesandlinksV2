0c068e828b26d18ed4e184a8508c45ce62927c4e
sakshamgorey <90107977+sakshamgorey@users.noreply.github.com>
2021-09-13 15:00:00 +0530

    Add files via upload

---
+++ assets/js/classesvoicecommands.js
@@ -0,0 +1,31 @@
+var cnthlms 
+function cnth(){
+    var cnth_lms = "http://lms.nuv.ac.in/course/view.php?id=390";
+    var cnth_ch="https://teams.microsoft.com/l/channel/19%3aIT5aGYrJMU8fVJQTctyq8ebyzVh6TbkuolIz-Q90TKs1%40thread.tacv2/General?groupId=08b6c487-e3d0-4c1f-ab50-8a657d51c7e9&tenantId=d1b99a31-59dc-4039-b9c4-f6166b6bf0a4";
+    var cnth_m="https://teams.microsoft.com/l/meetup-join/19%3Ameeting_ZThjYjNhYTktMzJiOS00NTg4LWJjOTEtZTFkZjVlNzljNzEz%40thread.v2/0?context=%7B%22Tid%22%3A%22d1b99a31-59dc-4039-b9c4-f6166b6bf0a4%22%2C%22Oid%22%3A%2290f88b24-8224-4466-9517-b4d27e276988%22%7D";
+        var x =window.open(cnth_ch,'_blank');
+        setTimeout(function() { x.close(); }, 1000);
+        var y =window.open(cnth_m,'_blank');
+        setTimeout(function() { y.close(); }, 1000);
+        cnthlms =window.open(cnth_lms,'_blank');
+    };
+function closecnth(){
+        cnthlms.close();
+    };   
+if (annyang) {
+
+    const commands = {
+        'cn': () => { cnth(); },
+        'close cn': () => { closecnth(); },
+        
+        
+        
+    };
+    
+    
+    // Add our commands to annyang
+    annyang.addCommands(commands);
+    
+    // Start listening.
+    annyang.start();
+    }
---
+++ assets/js/clock.js
@@ -0,0 +1,48 @@
+setInterval(showTime, 1000);
+        function showTime() {
+            let time = new Date();
+            let hour = time.getHours();
+            let min = time.getMinutes();
+            let sec = time.getSeconds();
+            var dd = String(time.getDate()).padStart(2, '0');
+            var mm = String(time.getMonth() + 1).padStart(2, '0'); //January is 0!
+            var yyyy = time.getFullYear();
+            var d = new Date();
+var weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+var n = weekday[d.getDay()];
+            am_pm = "AM";
+  
+            if (hour >= 12) {
+                hour -= 12;
+                am_pm = "PM";
+            }
+            if (hour == 0) {
+                hr = 12;
+                am_pm = "AM";
+            }
+  
+            hour = hour < 10 ? "0" + hour : hour;
+            min = min < 10 ? "0" + min : min;
+            sec = sec < 10 ? "0" + sec : sec;
+            
+            today = dd + '.' + mm + '.' + yyyy;
+            let currentTime = hour + ":" 
+                + min + am_pm ;
+  
+            document.getElementById("clock")
+                .innerHTML = currentTime;
+            document.getElementById("clock2")
+                .innerHTML = today;
+            document.getElementById("day")
+                .innerHTML = n;
+        }
+  
+        showTime();
